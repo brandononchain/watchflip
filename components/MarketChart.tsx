1b1bec02027cd507ea76d99f5166c53137af57ac
Brandon M. Crenshaw <109445618+brandononchain@users.noreply.github.com>
2026-05-25 23:47:10 -0500

    Add market chart component shell

---
+++ components/MarketChart.tsx
@@ -0,0 +1,8 @@
+export default function MarketChart() {
+  return (
+    <div className="card">
+      <h2>Market Trend</h2>
+      <div style={{ height: 220, borderRadius: 12, background: '#1b2028' }} />
+    </div>
+  )
+}
